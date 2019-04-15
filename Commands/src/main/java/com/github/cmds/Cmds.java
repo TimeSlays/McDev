bf05490beed348ead9fa0855575047fb25b89b39
BuildTools <unconfigured@null.spigotmc.org>
2019-04-15 12:47:49 -0400

    Initial Commit

--- Commands/src/main/java/com/github/cmds/Cmds.java
+++ Commands/src/main/java/com/github/cmds/Cmds.java
@@ -1,14 +1,17 @@
 package com.github.cmds;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Cmds extends JavaPlugin {
-
+public class Cmds extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getConsoleSender().sendMessage("Cmd Works");
         getCommand("addXp").setExecutor(new addXpClass());
+        getCommand("eft").setExecutor(new CircleEffect());
 
+
+        }
     }
-}
+
