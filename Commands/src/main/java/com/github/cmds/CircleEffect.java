899e885572816ca59918b26cb4ae84a6f88814f7
BuildTools <unconfigured@null.spigotmc.org>
2019-04-15 12:46:05 -0400

    Initial Commit

---
+++ Commands/src/main/java/com/github/cmds/CircleEffect.java
@@ -0,0 +1,31 @@
+package com.github.cmds;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CircleEffect implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = (Player) sender;
+        if (args.length == 1) {
+            if (args[0].equals("tp")) {
+                player.sendMessage("Effect Started");
+                Location loc = player.getLocation();
+                player.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
+
+
+            }
+
+        }
+        return true;
+    }
+}
+
